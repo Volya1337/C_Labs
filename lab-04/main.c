08e348a2906c021549112fa43975115b2ee6917d
Volya1337 <31896865+Volya1337@users.noreply.github.com>
2018-01-07 02:50:23 +0300

    Update main.c

--- lab-04/main.c
+++ lab-04/main.c
@@ -5,12 +5,12 @@ int main() {
     double mas[100];
     double a, x, d, h, xmin, xmax, maxv, minv;
     int c, s, i;
-    
+
     do{
 /*
  * Выбор формулы для дальнейшего расчета
  */
-    printf("Для выхода из программы, введите 0\n");
+        printf("Для выхода из программы, введите 0\n");
         printf("Формула 1: Рассчет G, введите 1\n");
         printf("Формула 2: Рассчет F, введите 2\n");
         printf("Формула 3: Рассчет Y, введите 3\n");
@@ -52,11 +52,11 @@ int main() {
             printf("Введите число шагов еще раз: ");
             scanf("%d", &s);
         }
-    if (s > 100){
-        printf("Ошибка с массивом");
-        return 0;
-    }
-            h = (xmax - xmin) / (s - 1);
+        if (s > 100){
+            printf("Массив переполнен");
+            return 0;
+        }
+        h = (xmax - xmin) / (s - 1);
 
         /*
          * Выполнение вычислений исходя из выбранной формулы
@@ -72,7 +72,7 @@ int main() {
                         break;
                     }
                     mas[i] = (-2 * (-5 * pow(a, 2) + 3 * a * x + 2 * pow(x, 2))) /
-                        (5 * pow(a, 2) + 9 * a * x - 2 * pow(x, 2));
+                             (5 * pow(a, 2) + 9 * a * x - 2 * pow(x, 2));
                     printf("x = %lf\t", x );
                     printf("G=%lf\n", mas[i]);
 
@@ -81,14 +81,14 @@ int main() {
                 case 2: {
                     printf("Введите а: ");
                     scanf("%lf", &a);
-                    for (x = xmin, i = 0; x < xmax; x+=h, i++) {
+                    for (x = xmin, i = 0; x <= xmax & i <= s; x+=h, i++) {
                         d = 5 * pow(a, 2) + 9 * a * x - 2 * pow(x, 2);
                         if (d == (10e-6)) {
                             printf("Введенные значения не входят в область определния ф-ции\n");
                             break;
                         }
                         mas[i] = sin(M_PI * (10.0 * pow(a, 2.0) + 37.0 * a * x + 7.0 * pow(x, 2.0))) /
-                            (M_PI * (10.0 * pow(a, 2.0) + 37.0 * a * x + 7.0 * pow(x, 2.0)));
+                                 (M_PI * (10.0 * pow(a, 2.0) + 37.0 * a * x + 7.0 * pow(x, 2.0)));
                         printf("x = %0.18lf\t", x );
                         printf("F= %0.18lf\n", mas[i]);
                     }
@@ -100,27 +100,28 @@ int main() {
                             printf("a должно быть отрицательным числом, введите его еще раз: ");
                             scanf("%lf", &a);
                         }
-                        for (x = xmin, i = 0; x < xmax; x+=h, i++) {
+                        for (x = xmin, i = 0; x <= xmax & i <= s; x+=h, i++) {
                             if (x < 0 || a < 0) {
+                                mas[i] = (log(-5 * pow(a, 2) - 16 * a * x + 16 * pow(x, 2) + 1)) / log(2);
+                                printf("x = %lf\t", x);
+                                printf("%lf\n", mas[i]);
                             }
-                            mas[i] = (log(-5 * pow(a, 2) - 16 * a * x + 16 * pow(x, 2) + 1)) / log(2);
-                            printf("x = %lf\t", x );
-                            printf("%lf\n", mas[i]);
                         }
                         break;
-
                     }
                 }
             }
         }
-for (i = 0, minv = mas[0], maxv = mas[0]; i <= s; i++){
-    if(mas[i] > maxv)
-        maxv = mas[i];
-    if (mas[i] < minv)
-        minv = mas[i];
-}
-    printf("Макс. значение: %0.18lf\n", maxv);
-    printf("Мин. значение: %0.18lf\n", minv);
+        for (i = 0, minv = mas[0], maxv = mas[0]; i < s; i++) {
+            if (mas[i] > maxv) {
+                maxv = mas[i];
+            }
+            if (mas[i] < minv) {
+                minv = mas[i];
+            }
+        }
+        printf("Макс. значение: %0.18lf\n", maxv);
+        printf("Мин. значение: %0.18lf\n", minv);
     } while (c != 0);
     return 0;
 }
